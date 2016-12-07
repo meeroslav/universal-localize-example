9fe3720be640f54cddc819e3e7fae239d2ee10d2
Miroslav Jonas <meeroslav@yahoo.com>
2016-12-07 22:48:15 +0100

    Fix AOT issue and update the localize-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
-import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader, TranslateService } from 'ng2-translate';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
-import { LocalizeRouterModule } from 'localize-router';
+import { LocalizeRouterModule, StaticParserLoader, LocalizeParser } from 'localize-router';
 
 import { AppComponent } from './app.component';
 
 export function createTranslateLoader(http: Http) {
   return new TranslateStaticLoader(http, '/assets/locales', '.json');
+}
+
+export function localizeLoaderFactory(translate: TranslateService, http: Http) {
+  return new StaticParserLoader(translate, http);
 }
 
 export const routes: Routes = [
@@ -31,7 +35,11 @@ export const routes: Routes = [
       deps: [Http]
     }),
     RouterModule.forRoot(routes),
-    LocalizeRouterModule.forRoot(routes),
+    LocalizeRouterModule.forRoot(routes, {
+      provide: LocalizeParser,
+      useFactory: localizeLoaderFactory,
+      deps: [TranslateService, Http]
+    }),
     HomeModule
   ],
   providers: [],
